73f24d92261f5d170a48f8ff19869afd2bcb3c22
ginga9003 <ginga9003@gmail.com>
2015-09-29 10:19:55 +0000

    heroku error clear

--- app/models/want.rb
+++ app/models/want.rb
@@ -2,6 +2,6 @@ class Want < Ownership
   
     # ランキングの指定件数を返す
   def self.rank_items(limit=10)
-     Want.all.group("ownerships.item_id").order('count_all desc, created_at desc').limit(limit).count()
+     Want.all.group("ownerships.item_id").order('count_all desc, ownerships.created_at desc').limit(limit).count()
   end
 end
