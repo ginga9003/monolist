4c83fe0d9d5558b6388baefa938c9b08d5cfef26
ginga9003 <ginga9003@gmail.com>
2015-09-29 10:16:04 +0000

    heroku error clear

--- app/models/have.rb
+++ app/models/have.rb
@@ -2,6 +2,6 @@ class Have < Ownership
   
   # ランキングの指定件数を返す
   def self.rank_items(limit=10)
-     Have.all.group(:item_id).order('count_all desc, created_at desc').limit(limit).count()
+     Have.all.group("ownerships.item_id").order('count_all desc, created_at desc').limit(limit).count()
   end
 end
