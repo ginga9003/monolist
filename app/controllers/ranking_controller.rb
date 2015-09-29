824e92f4f83a6559b02872960c422a4f1b173836
ginga9003 <ginga9003@gmail.com>
2015-09-29 09:47:48 +0000

    for Heroku

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -4,12 +4,41 @@ class RankingController < ApplicationController
   def have 
     #SELECT item_id, COUNT(id) AS count FROM ownerships WHERE type = 'Have' GROUP BY item_id ORDER BY count(id) DESC LIMIT 10;
     @rank_items = Have.rank_items(10)
+    @rank_items = rank(@rank_items)
   end
   
   # Wantランキング取得
   def want
     #SELECT item_id, COUNT(id) AS count FROM ownerships WHERE type = 'Want' GROUP BY item_id ORDER BY count(id) DESC LIMIT 10;
     @rank_items = Want.rank_items(10)
+    @rank_items = rank(@rank_items)
   end
   
+  # ランク付け
+  private
+  def rank(rank_items)
+    newRankItems = {}
+    
+    oldCount = 0
+    ranking = 1
+    totalRanking = 1
+    rank_items.each{|key, value|
+      
+      #binding.pry
+      if value != oldCount
+        # ランクを足す
+        ranking = totalRanking
+      end
+      
+      dataHash = {}
+      dataHash['count'] = value
+      dataHash['rank'] = ranking
+      newRankItems[key] = dataHash
+
+      totalRanking += 1
+      oldCount = value
+    }
+    
+    return newRankItems
+  end
 end
